e97b358972d528dc3fa7f5dc94f087dfe1c49eda
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2019-08-17 14:52:51 -0300

    TarefaExtra2
    Adicionado a tarefa extra 2

---
+++ TarefaExtra2/TarefaExtra-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+	//declaracao variaveis
+	float s = 0, n = 0, pot2 = 1, k = 1;
+	
+	//pega do usuario 'n'
+	scanf("%f", &n);
+	//n = 3.0;
+	
+	//enquanto 'k' for menor/igual que 'n'
+	while (k <= n)
+	{
+		pot2 = 2*pot2; //potencia = 2 vezes a potencia
+		float den = ((2*k)-1)*((2*k)+1); // denominador (2xn+1) x (2xn-1)
+		s = s+(pot2/den); // soma dos termos da sequencia
+		k += 1; // adc 1 ao contador
+	}
+	
+	printf("\n%f", s); //printa em tipo 'float' a soma;
+}
+
