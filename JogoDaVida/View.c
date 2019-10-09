99f4e50ba17df7bbf6fca7c4e224fa85f1b4e659
bixpo <gabriel@zaninotti.net>
2019-10-09 18:16:47 -0300

    progresso
    progresso na build da view da matrix porem nao finalizada ainda

--- JogoDaVida/View.c
+++ JogoDaVida/View.c
@@ -16,20 +16,13 @@ int EntradaInteiro(char valor[]){
 }
 
 void ExibirGeracao(tipo_Individuo matriz[100][100], int tamanho){
-	printf("   ");
-	for (int a = 1; a<=tamanho; a++)
-	{
-		printf("  %d", a);
-	}
-	printf("\n");
-	for(int i = 0; i<tamanho; i++)
-	{
-		printf("%d  ", i + 1);
-		
-		for(int j = 0; j<tamanho; j++)
-		{
-			printf("  %d", matriz[i][j].vivo);
-		}
-		printf("\n");
+	for (int a = 0; a<tamanho; a++){
+		printf("\t%d", a + 1);
+	}printf("\n");
+	for(int i = 0; i<tamanho; i++){
+		printf("%d ", i + 1);
+		for(int j = 0; j<tamanho; j++){
+			printf("\t%d ", matriz[i][j].vivo);
+		}printf("\n");
 	}		
 }
